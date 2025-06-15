82d4ce288e214ed3465a9c81c1431cda57c1f6be
Dylan <dylan@seedspider.com>
2025-06-15 05:19:17 +0000

    started a simple terminal drawing library

---
+++ include/term_draw.h
@@ -0,0 +1,2 @@
+
+int put_char(char c, char* text_colour, char* bg_colour);
---
+++ src/term_draw.c
@@ -0,0 +1,9 @@
+#include <ansi_codes.h>
+
+int _print_cmd_create(){
+	return 0;
+}
+
+int put_char(char c, char* text_colour, char* bg_colour){
+	return 0;
+}
